2d034faec0004dda0ff9adc3a38ffc4465e47812
O1Wilson <Badgermouse583@gmail.com>
2025-04-29 14:32:29 -0500

    Initial Commit

---
+++ hello.cpp
@@ -0,0 +1,21 @@
+#include <glad/glad.h>
+#include <glfw3.h>
+#include <iostream>
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // MacOS X Line
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "HellowWindow", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window!" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	return 0;
+}
